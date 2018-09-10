aa4ee1901c596b56514537f252f7c2954c4f7f0d
Osvaldo Fernandez <osvaldofernandez@KAIS-MAC43.fritz.box>
2018-09-10 16:03:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
